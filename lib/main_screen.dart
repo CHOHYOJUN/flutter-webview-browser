7cb4b6ba7f9c2636262fab00b71a698ef6c30ecc
Hyojun Cho <lionjerher84@gmail.com>
2023-12-21 00:54:15 +0900

    네이버 등 접속 허용 처리, appbar 메뉴 추가

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -20,7 +20,32 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('나만의 웹 브라우저')),
+      appBar: AppBar(
+        title: const Text('나만의 웹 브라우저'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+          ),
+          // +   ,  Icons.more_vert 햄버거 아이콘
+          PopupMenuButton<String>(
+            onSelected: (value){
+              print(value);
+            },
+              itemBuilder: (context) => [
+                    const PopupMenuItem(
+                        value: 'https://google.com',
+                        child: Text(
+                          '구글',
+                        )),
+                    const PopupMenuItem(
+                        value: 'https://naver.com',
+                        child: Text(
+                          '네이버',
+                        ))
+                  ]),
+        ],
+      ),
       body: const WebView(
         initialUrl: 'https://www.naver.com',
       ),
